8ab49f9565cead8a19c9d1a9acda0d1da4ac5629
A-ER <45385377+A-ER@users.noreply.github.com>
2018-11-27 12:55:39 +0100

    WHole project with asset files

---
+++ Play_and_Learn_Game/Assets/LongClickButton.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class LongClickButton : MonoBehaviour, IPointerDownHandler, IPointerUpHandler {
+
+    public UnityEvent onClick;
+    public UnityEvent onRelease;
+
+    bool clicked = false;
+    bool iterationLimit = true;
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        clicked = true;
+        iterationLimit = true;
+        Debug.Log("down");
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        Reset();
+        Debug.Log("up");
+    }
+
+    void Update()
+    {
+        if (clicked)
+        {
+            if (onClick != null)
+            {
+                onClick.Invoke();
+            }
+        } else if (iterationLimit)
+        {
+            if (onRelease != null)
+            {
+                onRelease.Invoke();
+                iterationLimit = false;
+            }
+        }
+    }
+
+    void Reset()
+    {
+        clicked = false;
+    }
+
+}
