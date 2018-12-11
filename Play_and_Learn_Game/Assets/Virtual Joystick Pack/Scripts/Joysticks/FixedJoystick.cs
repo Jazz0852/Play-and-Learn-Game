df50af520d3dcef24f2959ada214281192ee9807
Jazz0852 <43743540+Jazz0852@users.noreply.github.com>
2018-12-11 21:18:48 +0100

    Revert "Last changes JAzz"
    This reverts commit b7cefe3af27adfd906e4c9dd0ecf7519546a09e1.

--- Play_and_Learn_Game/Assets/Virtual Joystick Pack/Scripts/Joysticks/FixedJoystick.cs
+++ Play_and_Learn_Game/Assets/Virtual Joystick Pack/Scripts/Joysticks/FixedJoystick.cs
@@ -3,10 +3,7 @@ using UnityEngine.EventSystems;
 
 public class FixedJoystick : Joystick
 {
-    [Header("Fixed Joystick")]
-
-
-    Vector2 joystickPosition = Vector2.zero;
+    public Vector2 joystickPosition = Vector2.zero;
     private Camera cam = new Camera();
 
     void Start()
@@ -18,6 +15,7 @@ public class FixedJoystick : Joystick
     {
         Vector2 direction = eventData.position - joystickPosition;
         inputVector = (direction.magnitude > background.sizeDelta.x / 2f) ? direction.normalized : direction / (background.sizeDelta.x / 2f);
+        ClampJoystick();
         handle.anchoredPosition = (inputVector * background.sizeDelta.x / 2f) * handleLimit;
     }
 
@@ -28,7 +26,7 @@ public class FixedJoystick : Joystick
 
     public override void OnPointerUp(PointerEventData eventData)
     {
-        inputVector = Vector2.zero;
-        handle.anchoredPosition = Vector2.zero;
+       Vector2 inputVector = Vector2.zero;
+       handle.anchoredPosition = Vector2.zero;
     }
 }
